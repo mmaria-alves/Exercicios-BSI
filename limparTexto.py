b82c029eae164cf46e80fd6dd78a9e035df4bb1f
Maria Eduarda <mariaeduardaalves0615@gmail.com>
2026-08-28 21:33:47 -0300

    ajustes finais na função limparTexto

--- limparTexto.py
+++ limparTexto.py
@@ -1,40 +1,47 @@
-mensagem_teste = "Mas há front2eiras no5s jar8dins da razão"
-c_esp_A = ["á", "à", "ã", "ä"]
-c_esp_E = ["é", "è", "ẽ", "ë"]
-c_esp_I = ["í", "ì", "ĩ", "ï"]
-c_esp_O = ["ó", "ò", "õ", "ö"]
-c_esp_U = ["ú", "ù", "ũ", "ü"]
+# separação dos caracteres especiais
+caracteres_especiais = [",", ".", ";", ":", "!", "?", "_", "-", "+", "=", "@", "&", "/"]
+c_esp_A = ["á", "à", "ã", "ä", "â"]
+c_esp_E = ["é", "è", "ẽ", "ë", "ê"]
+c_esp_I = ["í", "ì", "ĩ", "ï", "î"]
+c_esp_O = ["ó", "ò", "õ", "ö", "ô"]
+c_esp_U = ["ú", "ù", "ũ", "ü", "û"]
 c_esp_Ç = ["c"]
 
 
-def limparTexto(mensagem_teste):
-    mensagem_teste = mensagem_teste.replace(" ", "") # para remover os espaços
+def limparTexto(mensagem_poluida):
+    # para remover os espaços
+    mensagem_poluida = mensagem_poluida.replace(" ", "")
 
-    # para remover as letras acentuadas
-    for char in mensagem_teste:
-        if char in c_esp_A:
-            mensagem_teste = mensagem_teste.replace(char, "a")
+    # para remover as letras acentuadas e caracteres especiais
+    for char in mensagem_poluida:
+        if char in caracteres_especiais:
+            mensagem_poluida = mensagem_poluida.replace(char, "")
+        elif char in c_esp_A:
+            mensagem_poluida = mensagem_poluida.replace(char, "a")
         elif char in c_esp_E:
-            mensagem_teste = mensagem_teste.replace(char, "e")
+            mensagem_poluida = mensagem_poluida.replace(char, "e")
         elif char in c_esp_I:
-            mensagem_teste = mensagem_teste.replace(char, "i")
+            mensagem_poluida = mensagem_poluida.replace(char, "i")
         elif char in c_esp_O:
-            mensagem_teste = mensagem_teste.replace(char, "o")
+            mensagem_poluida = mensagem_poluida.replace(char, "o")
         elif char in c_esp_U:
-            mensagem_teste = mensagem_teste.replace(char, "u")
+            mensagem_poluida = mensagem_poluida.replace(char, "u")
         elif char in c_esp_Ç:
-            mensagem_teste = mensagem_teste.replace(char, "c")
+            mensagem_poluida = mensagem_poluida.replace(char, "c")
 
-    lista_mensagem = list(map(str, mensagem_teste))
-
+    # para remover os números
+    lista_mensagem = list(map(str, mensagem_poluida))
     for char in lista_mensagem:
-        # limpar números
         if char.isnumeric():
             lista_mensagem.remove(char)
-    
-    mensagem_limpa = "".join(lista_mensagem)        
+
+    mensagem_limpa = "".join(lista_mensagem)
     return mensagem_limpa
 
+# mensagens testes
+mensagem_teste1 = "Mas há front2eiras no5s jar8dins da razão"
+mensagem_teste2 = "E3ssá lua cheìa 9traz u_m sentim7ento"
 
 mensagem = input().lower()
+
 print(limparTexto(mensagem))
